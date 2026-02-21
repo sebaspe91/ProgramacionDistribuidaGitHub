ade827bacb2531f62786de63ef21534cafd74fd5
sebaspe91 <sebaspegu91@hotmail.com>
2026-02-20 21:21:36 -0500

    contador banco sesion 1

--- clase1/servidor.py
+++ clase1/servidor.py
@@ -1,14 +1,37 @@
 
 import socket
+import threading
+
+def handle_client(conn, addr):
+    print(f"Cliente conectado dede {addr}")
+
+    try:
+        student_name = conn.recv(1024).decode()
+        response = f"Hola {student_name}, estas conectado a un servidor concurrente"
+        conn.sendall(response.encode())
+    except Exception as e:
+        print(f"Error con {addr}: {e}")
+    finally:
+        conn.close()
+        print(f"Conexion cerrada con {addr}")
+
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("127.0.0.1", 5000))
-server.listen(1)
+server.listen()
 
-print("Servidor esperando conexion...")
+print("Servidor concurrente escuchando...")
 
-conn, addr = server.accept()
-print("Cliente conectado:", addr)
+while True:
+    conn, addr = server.accept()
 
-conn.sendall(b"Juan Sebastian Perez")
-conn.close()
+    # crate a thread per client
+    client_thread = threading.Thread(
+        target=handle_client,
+        args=(conn, addr)
+    )
+
+
+    client_thread.start()
+# conn.close()
---
+++ clase2/servidor.py
@@ -0,0 +1,43 @@
+
+import socket
+import threading
+
+# contador del cliente
+contador_clientes = 0  # recurso compartido
+
+def handle_client(conn, addr):
+
+    global contador_clientes
+
+    student_name = conn.recv(1024).decode()
+
+    # Incrlemento contador
+    contador_clientes += 1
+
+    print(f"Cliente {contador_clientes} atenido desde {addr}")
+        
+    response = f"Hola {student_name}, eres el cliente nuemro {contador_clientes}"
+    conn.sendall(response.encode())
+
+    conn.close()
+
+
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("127.0.0.1", 5000))
+server.listen()
+
+print("Servidor concurrente con contador...")
+
+while True:
+    conn, addr = server.accept()
+
+    # crate a thread per client
+    client_thread = threading.Thread(
+        target=handle_client, 
+        args=(conn, addr)
+    )
+
+    client_thread.start()
+# conn.close()
